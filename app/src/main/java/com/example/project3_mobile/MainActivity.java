5d496e77607f38aab8e1da0f8315a873fb03fccb
Lucas Kotowski <thedestroyerkotowski@gmail.com>
2022-05-17 14:36:03 -0700

    Edits to main activity and activity_main.xml

--- app/src/main/java/com/example/project3_mobile/MainActivity.java
+++ app/src/main/java/com/example/project3_mobile/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.project3_mobile;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +10,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_PageOptions);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Navigation, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
